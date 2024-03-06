ff30618155e7e878aae6ecc5901e1e9f9ad7c42b
shehrozamin1997 <160301169+shehrozamin1997@users.noreply.github.com>
2024-03-06 22:10:05 +0500

    updated

---
+++ chap123.js
@@ -0,0 +1,111 @@
+// chp 01 
+
+
+// Q 01
+// var abc = "hello to  my web"
+// alert(abc)
+
+// Q 02
+
+// alert("Error! Place enter a valid password")
+
+
+// Q 03
+
+// alert("Error! Place enter a \n valid password");
+
+// Q 04
+
+// alert("Hello");
+// alert("welcome to my web");
+
+// Q 05 
+
+// var num1 = "hello i can run javascript through console";
+// console.log(num1);
+
+// Q  06
+
+// alert("welcome to my web");
+
+
+// chp2
+
+//   Q1
+
+
+// var abc = "Muhammad shehroz"
+//  alert(abc);
+
+// // Q2
+//  var shehroz = "Muhammad shehroz"
+//  alert(shehroz);
+
+ //  Q3
+
+//  var message="Hello World"
+//  alert(message);
+
+ // Q4
+
+//  var name="shehroz"
+//  var age= "32"
+//  var skill="Certified Mobile Application Developer"
+//  alert(name);
+//  alert(age);
+//  alert(skill);
+
+ //  Q5
+
+//  var message="PLZZA \n PLZZ \n PLZ \n PL \n P"
+//  alert(message);
+
+//  Q6
+
+// email="shehrozamin786@gmail.com"
+// alert("This is my email address:"+email);
+
+// Q7
+
+// book ="A SMART WAY TO LEARN JavaScript"
+// alert("I Am Trying To Learn From The Book"  + book);
+
+
+// Q8
+
+// var message ="Yeah! I can write content through javascript"
+// document.write(message);
+
+
+ //  Q9
+
+//  var message="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//  alert(message);
+
+
+ // Cht 3
+
+//  Q1
+
+// age = 12
+// alert("I am"+age+"years old");
+
+//  Q2
+
+// var track = 12
+// alert("You have Visited This site"+track+"Time");
+
+// Q3
+
+// var birthYear =1989
+//  document.write("My Birth Year Is :"+birthYear+"<br>")
+//  document.write("Date Type Of My Declared Variable is Number");
+
+
+ //  Q4
+
+//  var visitorName="shehroz"
+//  var productTitle="Pencil"
+//  var Quaintity = 4
+//  document.write(visitorName+""+"Ordered"+""+Quaintity+""+"pencil on XYZ Store");
+
