9d5eb087314954eac6787f448c9e09ad2c53d8e2
shehrozamin1997 <160301169+shehrozamin1997@users.noreply.github.com>
2024-03-06 19:42:48 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// CHAPTER 1 
+// Q1
+
+// alert("HELLO THIS IS MY WEBSITE")
+
+
+
+// Q2
+
+// alert ("Error! Please enter valid password")
+
+// Q3
+// alert("Welcome to JS land \n Happy Coding!")
+
+// Q4
+// alert("Welcome to JS land")
+
+// alert(" Happy Coding!")
+
+
+// Q4  
+// console.log("Hello... I can run JS through my web browser's console")
+
